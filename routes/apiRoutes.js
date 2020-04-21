e35fd1f9f4151f7c82c08cb2a336588df137709c
Karla Dayhoff <kddayhoff@gmail.com>
2020-04-21 18:33:46 -0500

    notes are now staying on page and pushing into .json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,43 @@
-const dataBase = require("../db/db.json");
-
+const uuidv4 = require("uuid/v4");
+const util = require("util");
+const fs = require("fs");
+const writeFileAsync = util.promisify(fs.writeFile);
+let noteData = require("../db/db.json");
 
 module.exports = function(app) {
- 
 
+  // GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
   app.get("/api/notes", function(req, res) {
-    res.json(dataBase);
-  });
-  
-  app.post("/api/notes", function(req, res) {  
-    res.json(dataBase);
+    // console.log(res.notes);
+    // let getNotesFromServer = JSON.parse(fs.readFileSync("./db/db.json", {encoding: "utf8"})); /* Inside the get function */
+    res.json(noteData);
   });
 
+
+  app.post("/api/notes", function(req, res) {
+    // console.log(res.notes);
+
+    let note = req.body;
+    let id = uuidv4();
+    note.id = id;
+    
+    noteData.push(note);
+    let returnNotesToServer = JSON.stringify(noteData);
+    writeFileAsync("./db/db.json", returnNotesToServer) 
+    .then (function () {
+      res.json(note);
+    })
+    .catch( function(error){
+      console.log(error);
+    })
+      
+  });
+
+
+
+  // * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+  //write file async - if true, for loop write to array that DO NOT match, noteData === keepNotes, then writefileAsync
+app.delete('/api/notes', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
 };
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 // Tells node that we are creating an "express" server
 const app = express();
-
+const uuid = require("uuid");
 
 // Sets an initial port. Will use this later in our listener
 const PORT = process.env.PORT || 3000;
@@ -14,48 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// require("./routes/apiRoutes")(app);
+// this connects our htmlRoutes and apiRoutes files to server.js
 require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
-
-//empty array for notes to be saved to 
-// const noteInput;
-// let noteData;
-// let newNote;
-
-//gets everything from db.json and pushes it to the page
-app.get("/api/notes", function(req, res) {
-    fs.readFile(path.join(__dirname, "./db/db.json"), function (err, data)
-    {
-        let noteData = JSON.parse(data)
-        res.JSON(noteData);
-    
-    });
-})
-
-// pushes to db.json from the page
-app.post("/api/notes", function(req, res) {
-    let newNote = req.body;
-    let id = uuid.v4();
-    newNote.id = `${id}`;
-    notes.push(newNote);
-    let saveNote = JSON.stringify(notes);
-    console.log(saveNote);
-    fs.writeFile(path.join(__dirname, "./db/db.json"),
-      function(err) {
-    {
-        if (err) throw err;
-       }
-    });
-        return res.JSON(newNote); 
-    });
-
-app.get("/api/notes", function(req,res) {
-    
-})
-// route deleting
-
-//unique uuid  if data === true 
 
 
 app.listen(PORT, function() {
