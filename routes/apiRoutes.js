abee5e7577855969e5dd52849896b80743698fc6
Karla Dayhoff <kddayhoff@gmail.com>
2020-04-23 15:54:20 -0500

    changed util.promisify to fs.read and write file -- promises not working. Able to delete data from .json with user click, but does not delete from page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const writeFileAsync = util.promisify(fs.writeFile);
 const readfileAsync = util.promisify(fs.readFile);
 let noteData = require("../db/db.json");
+const path = require("path");
 
 module.exports = function(app) {
 
@@ -38,24 +39,54 @@ module.exports = function(app) {
   //run for loop that goes through db.json data, looks for all the data that ISN"T selected by the user, write that to a new array then push to db.json using writefileasync
 app.delete('/api/notes/:id', function (req, res) {
 
-  // let id = uuidv4();
-  // note.id = id;
-  console.log("This is sorta working")
+  console.log("I am a fig tree")
+  // console.log(req.params.id);
+  
+  fs.readFile(path.join(__dirname, '../db/db.json'), function(err, data) {
+    if (err) throw err;
+    console.log(data);
+    deleteNote = JSON.parse(data)
+    deleteNote.splice(req.params.id, 1)
+
+    fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(deleteNote),function (err, data) {
+      if (err) throw err;
+      res.send();
+  })
+
+
+
+
+    // let returnNotesToServer = JSON.stringify(noteData);
+    
+    // writeFileAsync("./db/db.json", returnNotesToServer) 
+    // .then (function () {
+    //   res.send(noteData);
+    // })
+    // .catch( function(error){
+    //   console.log(error);
+    
+    // });
+
+  })
+})
+}
+
+
   
 
-  // let readNotes = JSON.parse(noteData);
+  
+
  
-  readfileAsync("./db/db.json", noteData)
-  .then (function () {
-    res.json(noteData);
-    
-  })
   
-  // for (let i = 0; i < deleteNote.length; i++);
-  // if ( noteData === true) {
-   
-  // }
-//use promisify to readfile
-  
-})
-};
+  // for (let i = 0; i < noteData.length; i++);
+  // if ( noteData.id === note) {
+  //   delete note;
+  //  let returnNotesToServer = JSON.stringify(noteData);
+  //   writeFileAsync("./db/db.json", returnNotesToServer) 
+  //   .then (function () {
+  //     res.json(note);
+  //   })
+  //   .catch( function(error){
+  //     console.log(error);
+  //   })
+
