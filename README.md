9614a322c0d05b6bbf1e66846dea9cbe3663017f
Karla Dayhoff <kddayhoff@gmail.com>
2020-04-23 23:17:21 -0500

    completed README for application. Still not removing deleted note from page. Up refreshing the note will disappear

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# express-note-taker
-This application will allow the user to take notes and have them stored to a server.
+# Express Note Taker
+This application is designed that a person can take a note with a note header and note body for everyday use. When a person no longer needs the note, he/she can delete it.
+
+## Technology
+
+Javascript, Node.js, express, heroku, and markdown(.md) - inquirer, util dependencies
+​​
+## Installation
+
+To install this code, clone the files from the repository https://github.com/kddayhoff/express-note-taker and open in your preferred code editor
+​
+## Usage 
+​Go to https://note-taker-kdd.herokuapp.com/. There the user will click on the home page button which takes them to a notes page. The pencil icon in the right hand corner will allow a user to enter in a note title and details in the body of the note. The "save" icon in the right corner will appear only after both fields have been completed. Click the icon, and the notes will not disappear, even if the page is refeshed. Clicking the trash can icon with delete the note permanently.
+
+## Collaborators
+​Every person in the VU Bootcamp Spring 2020. Without them none of this would be possible because I'd have quit by now.
+
+## Contact
+
+kddayhoff@gmail.com
+<br>
+https://github.com/kddayhoff 
+<br>
+www.linkedin.com/in/kddayhoff
+
+## License
+
+MIT License
+
+Copyright (c) [2020] [Karla Dayhoff]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+​
+​
+## Badges
+​
+Badges? We don't need no stinking badges
+​
+​
+## Contributing
+​
+Feel free to make this better. It's like Thoreau's <i>Walden</i>, nothing is original. Take an axe for example, you're not going to reinvent the axe. However, you can always leave it sharper than when you found it.
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,13 +2,12 @@ const { v4: uuidv4 } = require('uuid');
 const util = require("util");
 const fs = require("fs");
 const writeFileAsync = util.promisify(fs.writeFile);
-const readfileAsync = util.promisify(fs.readFile);
 let noteData = require("../db/db.json");
 const path = require("path");
 
 module.exports = function(app) {
 
-  // GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+//
   app.get("/api/notes", function(req, res) {
   
     res.json(noteData);
@@ -25,68 +24,37 @@ module.exports = function(app) {
 
     let returnNotesToServer = JSON.stringify(noteData);
     writeFileAsync("./db/db.json", returnNotesToServer) 
-    .then (function () {
-      res.json(note);
-    })
-    .catch( function(error){
-      console.log(error);
-    })
+      .then (function () {
+         res.json(note);
+      })
+      .catch( function(error){
+         console.log(error);
+      })
       
   });
 
-  //write file async - if true, for loop write to array that DO NOT match, noteData === keepNotes, then writefileAsync
-
-  //run for loop that goes through db.json data, looks for all the data that ISN"T selected by the user, write that to a new array then push to db.json using writefileasync
-app.delete('/api/notes/:id', function (req, res) {
-
-  console.log("I am a fig tree")
-  // console.log(req.params.id);
-  
-  fs.readFile(path.join(__dirname, '../db/db.json'), function(err, data) {
-    if (err) throw err;
-    console.log(data);
-    deleteNote = JSON.parse(data)
-    deleteNote.splice(req.params.id, 1)
-
-    fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(deleteNote),function (err, data) {
-      if (err) throw err;
-      res.send();
-  })
-
-
-
-
-    // let returnNotesToServer = JSON.stringify(noteData);
-    
-    // writeFileAsync("./db/db.json", returnNotesToServer) 
-    // .then (function () {
-    //   res.send(noteData);
-    // })
-    // .catch( function(error){
-    //   console.log(error);
-    
-    // });
-
-  })
-})
-}
-
+ app.delete('/api/notes/:id', function (req, res) {
 
   
+    fs.readFile(path.join(__dirname, '../db/db.json'), function(err, data) {
+     if (err) throw err;
+       let notes = JSON.parse(data)
+       let deletedNote = notes.splice(req.params.id, 1)
 
-  
+       console.log("===========")
+       console.log("this is the deleted data " + deletedNote)
+       console.log("===============")
+       console.log("This should be the saved data " +notes);
+       
+       fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes),    
+      function (err, data) {
+       
+        if (err) throw err;
+        res.send();
+      })
+     })
+   })
+ }
 
- 
-  
-  // for (let i = 0; i < noteData.length; i++);
-  // if ( noteData.id === note) {
-  //   delete note;
-  //  let returnNotesToServer = JSON.stringify(noteData);
-  //   writeFileAsync("./db/db.json", returnNotesToServer) 
-  //   .then (function () {
-  //     res.json(note);
-  //   })
-  //   .catch( function(error){
-  //     console.log(error);
-  //   })
+
 
